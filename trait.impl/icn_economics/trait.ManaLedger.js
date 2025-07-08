d16076a7eae9a571b1e42866043cae598af4689a
fahertym <fahertym@users.noreply.github.com>
2025-07-08 20:06:37 +0000

    deploy: 07c9919bfbe5fafb3b34efa7578708de9910564a

--- trait.impl/icn_economics/trait.ManaLedger.js
+++ trait.impl/icn_economics/trait.ManaLedger.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_economics",[]],["icn_runtime",[["impl <a class=\"trait\" href=\"icn_economics/trait.ManaLedger.html\" title=\"trait icn_economics::ManaLedger\">ManaLedger</a> for <a class=\"struct\" href=\"icn_runtime/context/struct.SimpleManaLedger.html\" title=\"struct icn_runtime::context::SimpleManaLedger\">SimpleManaLedger</a>"]]]]);
+    var implementors = Object.fromEntries([["icn_economics",[]],["icn_runtime",[["impl ManaLedger for <a class=\"struct\" href=\"icn_runtime/context/struct.SimpleManaLedger.html\" title=\"struct icn_runtime::context::SimpleManaLedger\">SimpleManaLedger</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[20,307]}
+//{"start":57,"fragment_lengths":[20,197]}
