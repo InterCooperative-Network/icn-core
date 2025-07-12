f45a7ea4476dc7d3ed94333b20b1335b568f4763
DownWithMatt <34975878+fahertym@users.noreply.github.com>
2025-07-11 20:46:26 -0400

    [dag] add root hash tracking

---
+++ crates/icn-dag/tests/root.rs
@@ -0,0 +1,33 @@
+use icn_common::{compute_merkle_cid, DagBlock, DagLink, Did};
+use icn_dag::compute_dag_root;
+
+fn make_block(id: &str, links: Vec<DagLink>) -> DagBlock {
+    let data = format!("{id}").into_bytes();
+    let ts = 0u64;
+    let author = Did::new("key", "tester");
+    let sig = None;
+    let cid = compute_merkle_cid(0x71, &data, &links, ts, &author, &sig, &None);
+    DagBlock {
+        cid,
+        data,
+        links,
+        timestamp: ts,
+        author_did: author,
+        signature: sig,
+        scope: None,
+    }
+}
+
+#[test]
+fn root_deterministic() {
+    let child = make_block("child", vec![]);
+    let link = DagLink {
+        cid: child.cid.clone(),
+        name: "child".into(),
+        size: 0,
+    };
+    let parent = make_block("parent", vec![link]);
+    let root1 = compute_dag_root(&[parent.cid.clone()]);
+    let root2 = compute_dag_root(&[parent.cid.clone()]);
+    assert_eq!(root1, root2);
+}
