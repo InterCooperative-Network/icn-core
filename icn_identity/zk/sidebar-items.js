ddfb352d91cb56bff9b12616c834eadbe6e5a7ca
fahertym <fahertym@users.noreply.github.com>
2025-07-13 01:43:21 +0000

    deploy: 54508349a53a1be7bfee6b108295e5eaa5b88c0d

--- icn_identity/zk/sidebar-items.js
+++ icn_identity/zk/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ZkError"],"struct":["BulletproofsProver","BulletproofsVerifier","DummyProver","DummyVerifier","Groth16Verifier"],"trait":["ZkProver","ZkVerifier"]};
+window.SIDEBAR_ITEMS = {"enum":["ZkError"],"struct":["BulletproofsProver","BulletproofsVerifier","DummyProver","DummyVerifier","Groth16Prover","Groth16Verifier"],"trait":["ZkProver","ZkVerifier"]};
