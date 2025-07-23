c53e46021e52f402ff5817c111e8a7e7dd68e1ba
fahertym <fahertym@users.noreply.github.com>
2025-07-23 22:59:19 +0000

    deploy: 548ece1e3e5f32a264e4df4abef3a22270ad659a

--- icn_governance/voting/sidebar-items.js
+++ icn_governance/voting/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["VotingError"],"struct":["BallotId","Candidate","CandidateId","Election","ElectionId","EligibilityRules","RankedChoiceBallot","RankedChoiceResult","RankedChoiceRound","Signature","VotingPeriod"],"trait":["BallotValidator","VotingSystem"]};
+window.SIDEBAR_ITEMS = {"enum":["VotingError"],"struct":["BallotAnchoringService","BallotId","Candidate","CandidateId","Election","ElectionId","EligibilityRules","RankedChoiceBallot","RankedChoiceResult","RankedChoiceRound","Signature","VotingPeriod"],"trait":["BallotValidator","VotingSystem"]};
