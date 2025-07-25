d6bef2d1ad1b7a286b58d4b660794f57c61d2aaa
fahertym <fahertym@users.noreply.github.com>
2025-07-25 05:26:06 +0000

    deploy: 2f88894d4334015d51dbec1d8558c1e7619d5b5d

--- trait.impl/icn_crdt/trait.CRDTValue.js
+++ trait.impl/icn_crdt/trait.CRDTValue.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_crdt",[]],["icn_governance",[["impl CRDTValue for <a class=\"enum\" href=\"icn_governance/crdt_proposal_state/enum.ProposalStatus.html\" title=\"enum icn_governance::crdt_proposal_state::ProposalStatus\">ProposalStatus</a>"],["impl CRDTValue for <a class=\"enum\" href=\"icn_governance/crdt_proposal_state/enum.VoteDecision.html\" title=\"enum icn_governance::crdt_proposal_state::VoteDecision\">VoteDecision</a>"],["impl CRDTValue for <a class=\"struct\" href=\"icn_governance/crdt_proposal_state/struct.ProposalMetadata.html\" title=\"struct icn_governance::crdt_proposal_state::ProposalMetadata\">ProposalMetadata</a>"],["impl CRDTValue for <a class=\"struct\" href=\"icn_governance/crdt_proposal_state/struct.Vote.html\" title=\"struct icn_governance::crdt_proposal_state::Vote\">Vote</a>"]]]]);
+    var implementors = Object.fromEntries([["icn_crdt",[]],["icn_governance",[["impl <a class=\"trait\" href=\"icn_crdt/trait.CRDTValue.html\" title=\"trait icn_crdt::CRDTValue\">CRDTValue</a> for <a class=\"enum\" href=\"icn_governance/crdt_proposal_state/enum.ProposalStatus.html\" title=\"enum icn_governance::crdt_proposal_state::ProposalStatus\">ProposalStatus</a>"],["impl <a class=\"trait\" href=\"icn_crdt/trait.CRDTValue.html\" title=\"trait icn_crdt::CRDTValue\">CRDTValue</a> for <a class=\"enum\" href=\"icn_governance/crdt_proposal_state/enum.VoteDecision.html\" title=\"enum icn_governance::crdt_proposal_state::VoteDecision\">VoteDecision</a>"],["impl <a class=\"trait\" href=\"icn_crdt/trait.CRDTValue.html\" title=\"trait icn_crdt::CRDTValue\">CRDTValue</a> for <a class=\"struct\" href=\"icn_governance/crdt_proposal_state/struct.ProposalMetadata.html\" title=\"struct icn_governance::crdt_proposal_state::ProposalMetadata\">ProposalMetadata</a>"],["impl <a class=\"trait\" href=\"icn_crdt/trait.CRDTValue.html\" title=\"trait icn_crdt::CRDTValue\">CRDTValue</a> for <a class=\"struct\" href=\"icn_governance/crdt_proposal_state/struct.Vote.html\" title=\"struct icn_governance::crdt_proposal_state::Vote\">Vote</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[15,787]}
+//{"start":57,"fragment_lengths":[15,1179]}
