7f1b83529e880112cad2e38d552e5ccfac7918cd
fahertym <fahertym@users.noreply.github.com>
2025-07-26 22:17:40 +0000

    deploy: 0a974c00cccdc787e43a053c7053d9bec5ff1984

--- trait.impl/icn_common/trait.Signable.js
+++ trait.impl/icn_common/trait.Signable.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_common",[]],["icn_governance",[["impl <a class=\"trait\" href=\"icn_common/trait.Signable.html\" title=\"trait icn_common::Signable\">Signable</a> for <a class=\"struct\" href=\"icn_governance/voting/struct.RankedChoiceBallot.html\" title=\"struct icn_governance::voting::RankedChoiceBallot\">RankedChoiceBallot</a>"]]]]);
+    var implementors = Object.fromEntries([["icn_common",[]],["icn_governance",[["impl Signable for <a class=\"struct\" href=\"icn_governance/voting/struct.RankedChoiceBallot.html\" title=\"struct icn_governance::voting::RankedChoiceBallot\">RankedChoiceBallot</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[17,308]}
+//{"start":57,"fragment_lengths":[17,208]}
