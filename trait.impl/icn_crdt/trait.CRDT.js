6f5f1915b863f23acecd0ca54b5ecc9c3fbc3adb
fahertym <fahertym@users.noreply.github.com>
2025-07-27 12:42:23 +0000

    deploy: aa818cccf6290ae9fa01479330544bf0f7498f9d

--- trait.impl/icn_crdt/trait.CRDT.js
+++ trait.impl/icn_crdt/trait.CRDT.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_crdt",[]],["icn_governance",[["impl <a class=\"trait\" href=\"icn_crdt/trait.CRDT.html\" title=\"trait icn_crdt::CRDT\">CRDT</a> for <a class=\"struct\" href=\"icn_governance/crdt_proposal_state/struct.ProposalCRDT.html\" title=\"struct icn_governance::crdt_proposal_state::ProposalCRDT\">ProposalCRDT</a>"]]]]);
+    var implementors = Object.fromEntries([["icn_crdt",[]],["icn_governance",[["impl CRDT for <a class=\"struct\" href=\"icn_governance/crdt_proposal_state/struct.ProposalCRDT.html\" title=\"struct icn_governance::crdt_proposal_state::ProposalCRDT\">ProposalCRDT</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[15,300]}
+//{"start":57,"fragment_lengths":[15,212]}
