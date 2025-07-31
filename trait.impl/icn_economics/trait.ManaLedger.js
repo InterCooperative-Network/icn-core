4dd119561f8c0d1554d475cc9eca541e003a6a35
fahertym <fahertym@users.noreply.github.com>
2025-07-31 21:05:30 +0000

    deploy: 6c81e8955a348b8f93cd01d40cbf8b8476a62ed5

--- trait.impl/icn_economics/trait.ManaLedger.js
+++ trait.impl/icn_economics/trait.ManaLedger.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_economics",[]],["icn_governance",[["impl ManaLedger for <a class=\"struct\" href=\"icn_governance/policy_testing/struct.MockManaLedger.html\" title=\"struct icn_governance::policy_testing::MockManaLedger\">MockManaLedger</a>"]]],["icn_runtime",[["impl <a class=\"trait\" href=\"icn_economics/trait.ManaLedger.html\" title=\"trait icn_economics::ManaLedger\">ManaLedger</a> for <a class=\"struct\" href=\"icn_runtime/context/mana/struct.SimpleManaLedger.html\" title=\"struct icn_runtime::context::mana::SimpleManaLedger\">SimpleManaLedger</a>"]]]]);
+    var implementors = Object.fromEntries([["icn_economics",[]],["icn_governance",[["impl <a class=\"trait\" href=\"icn_economics/trait.ManaLedger.html\" title=\"trait icn_economics::ManaLedger\">ManaLedger</a> for <a class=\"struct\" href=\"icn_governance/policy_testing/struct.MockManaLedger.html\" title=\"struct icn_governance::policy_testing::MockManaLedger\">MockManaLedger</a>"]]],["icn_runtime",[["impl <a class=\"trait\" href=\"icn_economics/trait.ManaLedger.html\" title=\"trait icn_economics::ManaLedger\">ManaLedger</a> for <a class=\"struct\" href=\"icn_runtime/context/mana/struct.SimpleManaLedger.html\" title=\"struct icn_runtime::context::mana::SimpleManaLedger\">SimpleManaLedger</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[20,214,318]}
+//{"start":57,"fragment_lengths":[20,324,318]}
