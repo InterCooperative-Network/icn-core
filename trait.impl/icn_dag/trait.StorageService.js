433901f08808401d146cb24c41f8db9503a5ab70
fahertym <fahertym@users.noreply.github.com>
2025-08-07 03:00:20 +0000

    deploy: daeba0c34d94b6ea3963def19922eaf442bb49dc

--- trait.impl/icn_dag/trait.StorageService.js
+++ trait.impl/icn_dag/trait.StorageService.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_dag",[]],["icn_runtime",[["impl <a class=\"trait\" href=\"icn_dag/trait.StorageService.html\" title=\"trait icn_dag::StorageService\">StorageService</a>&lt;DagBlock&gt; for <a class=\"struct\" href=\"icn_runtime/context/stubs/struct.StubDagStore.html\" title=\"struct icn_runtime::context::stubs::StubDagStore\">StubDagStore</a>"]]]]);
+    var implementors = Object.fromEntries([["icn_dag",[]],["icn_runtime",[["impl StorageService&lt;DagBlock&gt; for <a class=\"struct\" href=\"icn_runtime/context/stubs/struct.StubDagStore.html\" title=\"struct icn_runtime::context::stubs::StubDagStore\">StubDagStore</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[14,324]}
+//{"start":57,"fragment_lengths":[14,218]}
