6b6d616dbaec42a4b3e4fd4777496e841d38c914
fahertym <fahertym@users.noreply.github.com>
2025-08-07 11:00:12 +0000

    deploy: 93d85726b784fad8513640829683d54c1c0b51c0

--- trait.impl/icn_reputation/trait.ReputationStore.js
+++ trait.impl/icn_reputation/trait.ReputationStore.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["icn_governance",[["impl ReputationStore for <a class=\"struct\" href=\"icn_governance/policy_testing/struct.MockReputationStore.html\" title=\"struct icn_governance::policy_testing::MockReputationStore\">MockReputationStore</a>"]]],["icn_reputation",[]]]);
+    var implementors = Object.fromEntries([["icn_governance",[["impl <a class=\"trait\" href=\"icn_reputation/trait.ReputationStore.html\" title=\"trait icn_reputation::ReputationStore\">ReputationStore</a> for <a class=\"struct\" href=\"icn_governance/policy_testing/struct.MockReputationStore.html\" title=\"struct icn_governance::policy_testing::MockReputationStore\">MockReputationStore</a>"]]],["icn_reputation",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[233,22]}
+//{"start":57,"fragment_lengths":[355,22]}
